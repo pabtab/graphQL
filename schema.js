46b39f2cf37f371728aa9881633838887176e0e4
Pablo Tabares Hernandez <pablother3@gmail.com>
2017-10-20 14:25:40 -0500

    change dependency and create scheme

--- schema.js
+++ schema.js
@@ -1,13 +1,41 @@
 const { makeExecutableSchema } = require('graphql-tools')
 
 const typeDefs = `
+    # Curso en el sistema
     type Curso {
         id: ID!
         titulo: String!
+        # Descripcion del curso
+        descripcion: String!
+        profesor: Profesor
+        rating: Float
+        comentarios: [Comentario]
+    }
+
+    type Profesor {
+        id: ID!
+        nombre: String!
+        nacionalidad: String!
+        genero: Genero
+        cursos: [Curso]
+    }
+
+    enum Genero {
+        FEMENINO
+        MASCULINO
+    }
+
+    type Comentario {
+        id: ID!
+        nombre: String!
+        cuerpo: String!
     }
 
     type Query {
         cursos: [Curso]
+        profesores: [Profesor]
+        curso(id: Int): Curso
+        profesor(id: Int): Profesor
     }
 `
 
